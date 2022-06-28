6f7b64dfe8d334bcc0d2b3fae7ee502e5d7cc2c8
Brian <huynh.sbrian@gmail.com>
2022-06-28 08:29:03 -0700

    Done with assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function divide(){
+    return 2000 / 100;
+}
+
+function square(x = 2){
+    return x ** 2;
+} 
+
+function add(a = 3, b= 4){
+    console.log(`Adding ${a}`);
+    console.log(`Adding ${b}`);
+    return a + b;
+}
